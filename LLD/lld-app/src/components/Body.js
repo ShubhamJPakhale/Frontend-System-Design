cd6170f868b3a873d08b847b848530641814fee7
ShubhamJPakhale <pakhaleshubham42@gmail.com>
2025-10-05 19:17:25 +0530

    infinite scroll code - machine coding

--- LLD/lld-app/src/components/Body.js
+++ LLD/lld-app/src/components/Body.js
@@ -8,21 +8,40 @@ const Body = () => {
 
   useEffect(() => {
     fetchData();
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  // this function will be called when user scrolls to bottom of the page - then it will call the fetchData function to load more memes
+  const handleScroll = () => {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
+      fetchData();
+    }
+  };
+
   const fetchData = async () => {
-    setShowShimmer(true);
-    const result = await fetch("https://meme-api.com/gimme/20");
-    const data = await result.json();
-    setMemeData(data.memes);
-    setShowShimmer(false);
+    try {
+      setShowShimmer(true);
+      const result = await fetch("https://meme-api.com/gimme/20");
+      if (result.status !== 200) {
+        throw new Error("Failed to fetch memes");
+      }
+      const data = await result.json();
+      setShowShimmer(false);
+      setMemeData((prevMemeData) => [...prevMemeData, ...data.memes]);
+    } catch (error) {
+      setShowShimmer(false);
+      console.error("Error fetching meme data:", error);
+    }
   };
 
   return (
     <div className="flex flex-wrap gap-4 p-4">
-      {showShimmer && <Shimmer />}
       {memeData &&
-        memeData.map((meme) => <MemeCard meme={meme} key={meme.author} />)}
+        memeData.map((meme, i) => (
+          <MemeCard meme={meme} key={meme.author + i} />
+        ))}
+      {showShimmer && <Shimmer />}
     </div>
   );
 };
